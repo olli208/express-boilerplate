22fd6217c7475d6c06fceeb9a0bffdcfd899bfab
Oliver <oliveroenang@gmail.com>
2018-06-11 16:18:17 +0200

    server js

--- server.js
+++ server.js
@@ -10,20 +10,8 @@ var mongoose = require('mongoose');
 
 var app = express();
 var server = require('http').createServer(app);
-global.io = require('socket.io')(server);  
 
 require('dotenv').config(); // secret stuff
-
-mongoose.connect(process.env.DATABASE)
-mongoose.Promise = global.Promise;
-
-mongoose.connection.on('error' , function (err) {
-  console.log('Something went wrong with MONGODB ->' , err.message)
-})
-
-// My models ( ͡° ͜ʖ ͡°)
-require('./models/Game');
-require('./models/Chat');
 
 // Setup and middleware
 app.set('view engine' , 'ejs')
@@ -41,7 +29,6 @@ app.set('view engine' , 'ejs')
     resave: false, 
     saveUninitialized: true
   }))
-  .use(flash());
 
 app.use(function (req, res, next) {
   res.locals.h = helpers;
@@ -51,56 +38,6 @@ app.use(function (req, res, next) {
 
 // My Routes
 app.use('/', require('./routes/index'));
-
-// SOCKET Things here
-io.on('connect', function (socket) {
-  var Chat = mongoose.model('Chat');
-  var Game = mongoose.model('Game');
-
-  socket.on('chat message', function (data){
-    var ns = io.of(`/${data.gameID}`);
-    console.log(ns)
-
-    var formatChat = {
-      gameID: data.gameID,
-      message: 
-        {
-          content: data.message,
-          date:  data.date,
-          time: data.time
-        }
-    }
-
-    new Chat(formatChat)
-      .save()
-      .then(function (chat) {
-        console.log('NEW MESSAGE');
-        // ns.emit('new message', chat);
-        io.emit('new message', chat);
-      });
-  });
-  
-  socket.on('check ID', function (data) {
-    var games = Game.find({'gameID': { $in: data.gameIDs }});
-    
-    games
-      .then(function (games) {
-        var difference = diff(games, data); // Compare old with new data
-
-        if (difference) {
-          io.emit('games DB', { games });
-        }
-      })
-      .catch(function (err) {
-        console.log(`Could not find GAMES from DATABASE -> ${err}`)
-      })
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
-});
 
 server.listen(process.env.PORT, function (){
     console.log('server is running on: ' + process.env.PORT);
