170d3ed2cd11b9bbe85ec3b4576bb7d6c9b63cd5
Oliver <oliveroenang@gmail.com>
2019-01-02 16:36:27 +0100

    basic header

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var compression = require('compression');
-var diff = require('deep-diff').diff; 
 var session = require('express-session');
-var flash = require('connect-flash');
 var helpers = require('./helpers');
 var mongoose = require('mongoose');
 
@@ -32,13 +30,12 @@ app.set('view engine' , 'ejs')
 
 app.use(function (req, res, next) {
   res.locals.h = helpers;
-  res.locals.flashes = req.flash();
   next();
 })
 
 // My Routes
 app.use('/', require('./routes/index'));
 
-server.listen(process.env.PORT, function (){
-    console.log('server is running on: ' + process.env.PORT);
+server.listen(1000, function (){
+    console.log('server is running on: ' + 1000);
 })
